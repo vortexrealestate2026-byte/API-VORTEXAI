b096a5cfc4fe64585fec358a849a174273969f04
vortexrealestate2026-byte <vortexrealestate2026@gmail.com>
2026-03-17 12:34:52 -0500

    Update database.py

--- app/database/database.py
+++ app/database/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
